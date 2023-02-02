63ce02759c65d8a616a2ab4a5bdc17a4225e2326
cptemre <kunduraci2@gmail.com>
2023-02-02 12:32:06 +0100

    Project Upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# ALGORITHM AND DATA STRUCTURES LABORATORY
+
+## TASK 6
+
+### GENERAL INFORMATION
+
+1. This project has been prepared for a course in informatics in the first semester.
+
+2. Project is prepared in Visual Studio 2022 application in Windows 11 OS. It may not work as intended in other platforms.
+
+3. Project only contains the code in github not the project folder prepared by Visual Studio.
+
+### DESCRIPTION OF THE TASK
+
+Give an array of n elements, from the keyboard, the values ​​of the elements in a loop. Derive:
+a) the value of the first element of this array,
+b) content of the array,
+c) the difference between the last element and the first,
+d) the sum of all elements in the array,
+e) any element selected by the user.
---
+++ task-6.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    int input = 0;
+
+    vector<int> myList;
+    int counter = 0;
+
+    cout << "Enter a positive number for your list.\n";
+    cin >> input;
+
+    do
+    {
+        myList.push_back(input);
+        cout << "To stop giving inputs press 0.\nTo continue to input press any other number\n";
+        cin >> input;
+        counter++;
+    } while (input != 0);
+
+    counter--;
+    int a = myList.at(0);
+    int c = myList.at(counter) - myList.at(0);
+    int d = 0;
+    int e = 0;
+    int counter2 = 0;
+    int counter3 = 0;
+
+
+    while (counter2 != counter + 1)
+    {
+        d += myList.at(counter2);
+        counter2++;
+    }
+
+    cout << "Which list element would you like to see?\n";
+    cin >> counter3;
+
+    e = myList.at(counter3 - 1);
+
+    cout << "a- Value of the first element of this array: " << a << endl;
+    cout << "b- Content of the array: ";
+    for (int i = 0; i < myList.size(); i++)
+    {   
+        if (i != myList.size() - 1)
+        {
+            cout << myList.at(i) << ", ";
+        }
+        else {
+            cout << myList.at(i);
+        }
+    }
+    cout << endl << "c- The difference between the last element and the first: " << c << endl;
+    cout << "d- The sum of all elements in the array: " << d << endl;
+    cout << "e- The element selected by the user: " << e << endl;
+    return 0;
+}
